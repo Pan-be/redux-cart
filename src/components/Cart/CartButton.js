c20da84b960961c3128b47c2410d486cb72a9596
Konrad <konradbieniek@gmail.com>
2023-01-11 22:55:48 +0100

    configure cartSlice and used it

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,16 +1,20 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { uiActions } from "../../store/uiSlice"
 import classes from "./CartButton.module.css"
 
 const CartButton = (props) => {
 	const dispatch = useDispatch()
+
+	const cartQuantiti = useSelector((state) => state.cart.totalQuantity)
+
 	const toggleCartBtn = () => {
 		dispatch(uiActions.toggle())
 	}
+
 	return (
 		<button className={classes.button} onClick={toggleCartBtn}>
 			<span>My Cart</span>
-			<span className={classes.badge}>1</span>
+			<span className={classes.badge}>{cartQuantiti}</span>
 		</button>
 	)
 }
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,19 +1,38 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem"
+import classes from "./Products.module.css"
+
+const PRODUCTS = [
+	{
+		id: "p1",
+		price: 5,
+		title: "my e-book",
+		description: "my very own e-book",
+	},
+	{
+		id: "p1",
+		price: 5,
+		title: "my mobile",
+		description: "my very own mobile",
+	},
+]
 
 const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
-};
+	return (
+		<section className={classes.products}>
+			<h2>Buy your favorite products</h2>
+			<ul>
+				{PRODUCTS.map((product) => (
+					<ProductItem
+						key={product.id}
+						id={product.id}
+						title={product.title}
+						price={product.price}
+						description={product.description}
+					/>
+				))}
+			</ul>
+		</section>
+	)
+}
 
-export default Products;
+export default Products
