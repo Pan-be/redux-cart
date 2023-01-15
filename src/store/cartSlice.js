f60fb7a56b8a81243134d672c37f1e03254f53f7
Konrad <konradbieniek@gmail.com>
2023-01-15 03:02:38 +0100

    move sendCartData function to cartAction file and make a fetch request function

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,17 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit"
-import { uiActions } from "./uiSlice"
 
 const cartSlice = createSlice({
 	name: "cart",
 	initialState: {
 		items: [],
 		totalQuantity: 0,
+		changed: false,
 	},
 	reducers: {
+		replaceCart(state, action) {
+			state.totalQuantity = action.payload.totalQuantity
+			state.items = action.payload.items
+		},
 		addItemToCart(state, action) {
 			const newItem = action.payload
 			const existingItem = state.items.find((item) => item.id === newItem.id)
 			state.totalQuantity++
+			state.changed = true
 			if (!existingItem) {
 				state.items.push({
 					id: newItem.id,
@@ -29,6 +34,8 @@ const cartSlice = createSlice({
 			const id = action.payload
 			const existingItem = state.items.find((item) => item.id === id)
 			state.totalQuantity--
+			state.changed = true
+
 			if (existingItem.quantity === 1) {
 				state.items = state.items.filter((item) => item.id !== id)
 			} else {
@@ -38,49 +45,6 @@ const cartSlice = createSlice({
 		},
 	},
 })
-
-export const sendCartData = (cart) => {
-	return async (dispatch) => {
-		dispatch(
-			uiActions.showNotification({
-				status: "pending",
-				title: "Sending...",
-				message: "Sending cart data.",
-			})
-		)
-		const sendRequest = async () => {
-			const response = await fetch(
-				"https://tasks-menager-default-rtdb.firebaseio.com/pseudoshop.json",
-				{
-					method: "PUT",
-					body: JSON.stringify(cart),
-				}
-			)
-
-			if (!response.ok) {
-				throw new Error("Sending cart data faild")
-			}
-		}
-		try {
-			await sendRequest()
-			dispatch(
-				uiActions.showNotification({
-					status: "success",
-					title: "Success!",
-					message: "Sent cart data successfully!",
-				})
-			)
-		} catch (error) {
-			dispatch(
-				uiActions.showNotification({
-					status: "error",
-					title: "Error!",
-					message: "Sending cart data failed!",
-				})
-			)
-		}
-	}
-}
 
 export const cartActions = cartSlice.actions
 
